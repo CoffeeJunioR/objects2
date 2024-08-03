35567562033fa2dc14f2a50b8c41a6e2d89fb42f
Denis Savanin <superrr.den@list.ru>
2024-08-03 16:24:06 +0300

    Some fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-export default function getSpecialAttack([attack1,attack2]) {
-    const array = [];
-    if ((!attack1.hasOwnProperty('description') && (!attack2.hasOwnProperty('description')))) {
-      attack2.description = 'Описание недоступно';
-      attack1.description = 'Описание недоступно';
-      array.push(attack1, attack2);
-    } else if (!attack2.hasOwnProperty('description')) {
-      attack2.description = 'Описание недоступно';
-      array.push(attack1,attack2);
-    } else {
-      array.push(attack1,attack2);
-    }
-    
-    return array;
-  }
+export default function getSpecialAttack({ special }) {
+  special.forEach((elem) => {
+    const {description = 'Описание недоступно'} = elem;
+    elem.description = description;
+  });
+  return special;
+}
